6ea3e97482e7f43732821c2f997b84b33ba5d5b7
Deepak-gusaiwal <94779081+Deepak-gusaiwal@users.noreply.github.com>
2022-03-16 00:59:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('this is a project');
+
+// window.addEventListener('scroll',()=>{
+//     let nav = document.querySelector('.abso');
+//     let scrollY = this.pageYOffset;
+//     let navTop = nav.offsetTop;
+//     console.log(navTop)
+//     if (window.scrollY >= navTop) {    
+//         nav.classList.add('fixed');
+//       } else {
+//         nav.classList.remove('fixed');    
+//       }
+//     console.log(navTop)
+// })
